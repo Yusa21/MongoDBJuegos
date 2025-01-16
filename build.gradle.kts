df59d17893ba071c16f5f93903c7f2e36979b80f
Yusa21 <157543456+Yusa21@users.noreply.github.com>
2025-01-16 21:19:07 +0100

    Primer commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    // https://mvnrepository.com/artifact/org.mongodb/mongodb-driver-kotlin-sync
+    implementation("org.mongodb:mongodb-driver-kotlin-sync:5.3.0")
+
+    // https://mvnrepository.com/artifact/io.github.cdimascio/dotenv-kotlin
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.2")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
